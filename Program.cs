11d717ecf8719ba675100ca77ad99de6596e51e8
Cappuccino <cappuccino@cappuccino093.com>
2023-11-10 04:02:01 -0600

    Initial test commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Random random = new();
+        int years = random.Next(0, 100);
+
+        if (years == 0)
+        {
+            Console.WriteLine($"See you tomorrow, {Environment.UserName}!");
+        }
+        else
+        {
+            Console.WriteLine($"See you in {years} years, {Environment.UserName}!");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
