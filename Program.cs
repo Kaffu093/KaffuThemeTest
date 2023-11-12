58913184e1ff4dc46a7099fcd848623cd08a8f57
Cappuccino <cappuccino@cappuccino093.com>
2023-11-12 05:41:44 -0600

    Add new files and update existing files for CappuccinoThemeTest project

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿namespace CappuccinoThemeTest;
+
+class Program
+{
+	static void Main()
+	{
+		// Greet the user
+		Console.WriteLine($"Hello {Environment.UserName}! Welcome to the calculator.");
+
+		// Ask the user to input two numbers
+		Console.Write("Enter the first number: ");
+		decimal num1;
+		try
+		{
+			num1 = Convert.ToDecimal(Console.ReadLine());
+		}
+		catch (FormatException)
+		{
+			Console.WriteLine("Invalid input. Please enter a valid decimal number.");
+			return;
+		}
+
+		Console.Write("Enter the second number: ");
+		decimal num2;
+		try
+		{
+			num2 = Convert.ToDecimal(Console.ReadLine());
+		}
+		catch (FormatException)
+		{
+			Console.WriteLine("Invalid input. Please enter a valid decimal number.");
+			return;
+		}
+
+		// Ask the user to select an operation
+		Console.WriteLine("Select an operation:");
+		Console.WriteLine("\t1. Add");
+		Console.WriteLine("\t2. Subtract");
+		Console.WriteLine("\t3. Multiply");
+		Console.WriteLine("\t4. Divide");
+
+		byte option;
+		try
+		{
+			option = Convert.ToByte(Console.ReadLine());
+		}
+		catch (FormatException)
+		{
+			Console.WriteLine("Invalid input. Please enter a valid option number.");
+			return;
+		}
+
+		// Perform the selected operation and display the result
+		switch (option)
+		{
+			case 1:
+				Console.WriteLine($"The result of the addition is: {num1 + num2}");
+				break;
+			case 2:
+				Console.WriteLine($"The result of the subtraction is: {num1 - num2}");
+				break;
+			case 3:
+				Console.WriteLine($"The result of the multiplication is: {num1 * num2}");
+				break;
+			case 4:
+				try
+				{
+					Console.WriteLine($"The result of the division is: {num1 / num2}");
+				}
+				catch (DivideByZeroException)
+				{
+					Console.WriteLine("Cannot divide by zero. Please enter a non-zero second number.");
+					return;
+				}
+				break;
+			default:
+				Console.WriteLine("Invalid option");
+				break;
+		}
+
+		// Wait for the user to press a key to exit
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey();
+	}
+}
---
+++ ReadMe.md
@@ -0,0 +1,47 @@
+# Cappuccino's Theme Test Project! ☕
+
+This project serves as a test for the Cappuccino's Theme.
+
+## Tools 🛠️
+
+- [.NET](https://dotnet.microsoft.com/) as the main framework
+- [Visual Studio Code](https://code.visualstudio.com/) as my main IDE/Text Editor
+- [Git](https://git-scm.com) as the control version system
+
+### Visual Studio Code Extensions 🧩
+
+- [C# Dev Kit](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit) to work with C# in Visual Studio Code
+- [Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker) to check the spelling in Visual Studio Code
+- [CSharpier](https://marketplace.visualstudio.com/items?itemName=chsword.csharpier) to format the C# code in Visual Studio Code
+- [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode) to format the code in Visual Studio Code
+
+## Use 🚀
+
+- Clone the repository
+- Open the project in Visual Studio Code
+- Run the project
+
+## Contributing 🤝
+
+If you think that this project needs more languages or some changes for testing the theme, you can open an issue or a pull request.
+
+If you want to open an issue, you can follow the next steps:
+
+- Go to the [issues](https://github.com/Cappuccino093/CappuccinoThemeTest/issues) section
+- Click on the `New issue` button
+- Fill the form with the information of the issue
+- Click on the `Submit new issue` button
+
+If you want to open a pull request, you can follow the next steps:
+
+- Fork the project
+- Create a new branch (`git checkout -b feature/AmazingFeature`)
+- Commit your changes (`git commit -m 'Add some AmazingFeature'`)
+- Push the changes (`git push origin feature/AmazingFeature`)
+- Open a Pull Request
+
+I'll be so grateful if you contribute to the project!
+
+## License 📄
+
+This project is under the [MIT License](License.md).
